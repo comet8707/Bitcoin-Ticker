53c6dce363e3ada461d4bba57633badfcf3d61c4
jun <junseok.kim8707@gmail.com>
2019-06-19 20:27:00 -0400

    index.js/html modified

--- index.js
+++ index.js
@@ -15,15 +15,26 @@ app.post("/", function(req, res){
   var crypto = req.body.crypto;
   var fiat = req.body.fiat;
 
-  var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-  var finalURL = baseURL + crypto + fiat;
-  request(finalURL, function(error, response, body){
-    var data = JSON.parse(body);
-    var price = data.last;
+  var amount = req.body.amount;
 
-    var currentDate = data.display_timestamp;
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    metohd: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+
+  request(options, function(error, response, body){
+    var data = JSON.parse(body);
+    var price = data.price;
+
+    var currentDate = data.time;
+
     res.write("<p>The current date is " + currentDate + "</p>");
-    res.write("<h1>The current price of " + crypto + " is " + price + fiat+"</h1>");
+    res.write("<h1>" +amount + crypto + " is currently worth " + price + fiat+"</h1>");
 
     res.send();
   });
